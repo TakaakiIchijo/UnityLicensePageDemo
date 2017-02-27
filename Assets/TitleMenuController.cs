b3f0d305afa1d4da550f159917ea453cf0d3d61e
Takaaki Ichijo <ttwcodeout@gmail.com>
2017-02-27 18:48:37 +0900

    最初のコミット

---
+++ Assets/TitleMenuController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TitleMenuController : MonoBehaviour
+{
+    public Canvas licenseCanvas;
+
+    private void Start()
+    {
+        HideLicenseCanvas();
+    }
+
+    public void ShowLicenseCanvas()
+    {
+        licenseCanvas.enabled = true;
+    }
+
+    public void HideLicenseCanvas()
+    {
+        licenseCanvas.enabled = false;
+    }
+}
